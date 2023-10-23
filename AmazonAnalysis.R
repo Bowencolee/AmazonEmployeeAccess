9776901d6868850e2e7c86c5909745e45fc4c9b1
Cole Bowen <bowencolee@gmail.com>
2023-10-23 13:21:50 -0600

    Updated with PCA recipe material

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -36,7 +36,9 @@ my_recipe <- recipe(ACTION~., data=amazon_train) %>%
               step_normalize(all_numeric_predictors()) %>%
               # step_other(all_nominal_predictors(), threshold = .001) %>% # combines categorical values that occur <5% into an "other" value
               # step_dummy(all_nominal_predictors()) # %>% # dummy variable encoding
-              step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) #target encoding
+              step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>% #target encoding
+              step_normalize(all_numeric_predictors()) %>%
+              step_pca(all_predictors(),threshold = .85) # principle component reduction
               # also step_lencode_glm(), step_lencode_bayes(), and step_lencode_mixed()
 
 
@@ -142,39 +144,39 @@ baked_recipe <- bake(prepped_recipe, amazon_test)
 #vroom_write(x=classForest_preds, file="./amazon_classForest.csv", delim=",")
 
 ##### Naive Bayes #####
-# 
-# nb_model <- naive_Bayes(Laplace=tune(), smoothness=tune()) %>%
-#               set_mode("classification") %>%
-#               set_engine("naivebayes")
-# 
-# nb_wf <- workflow() %>%
-#           add_recipe(my_recipe) %>%
-#           add_model(nb_model)
-# 
-# tuning_grid <- grid_regular(Laplace(),
-#                             smoothness(),
-#                             levels = 5)
-# 
-# folds <- vfold_cv(amazon_train, v = 5, repeats = 1)
-# 
-# CV_results <- nb_wf %>%
-#   tune_grid(resamples=folds,
-#             grid=tuning_grid,
-#             metrics=metric_set(roc_auc)) #f_meas,sens, recall,spec, precision, accuracy
-# 
-# bestTune <- CV_results %>%
-#   select_best("roc_auc")
-# 
-# final_wf <- nb_wf %>%
-#   finalize_workflow(bestTune) %>%
-#   fit(data=amazon_train)
-# 
-# nb_preds <- predict(final_wf, new_data=amazon_test,type="prob") %>%
-#   bind_cols(., amazon_test) %>% #Bind predictions with test data
-#   select(id, .pred_1) %>% #Just keep resource and predictions
-#   rename(Action=.pred_1)
 
-#vroom_write(x=nb_preds, file="./amazon_naiveBayes.csv", delim=",")
+nb_model <- naive_Bayes(Laplace=tune(), smoothness=tune()) %>%
+             set_mode("classification") %>%
+             set_engine("naivebayes")
+
+nb_wf <- workflow() %>%
+         add_recipe(my_recipe) %>%
+         add_model(nb_model)
+
+tuning_grid <- grid_regular(Laplace(),
+                            smoothness(),
+                            levels = 5)
+
+folds <- vfold_cv(amazon_train, v = 5, repeats = 1)
+
+CV_results <- nb_wf %>%
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=metric_set(roc_auc)) #f_meas,sens, recall,spec, precision, accuracy
+
+bestTune <- CV_results %>%
+  select_best("roc_auc")
+
+final_wf <- nb_wf %>%
+ finalize_workflow(bestTune) %>%
+ fit(data=amazon_train)
+
+nb_preds <- predict(final_wf, new_data=amazon_test,type="prob") %>%
+  bind_cols(., amazon_test) %>% #Bind predictions with test data
+  select(id, .pred_1) %>% #Just keep resource and predictions
+  rename(Action=.pred_1)
+
+vroom_write(x=nb_preds, file="./amazon_naiveBayes.csv", delim=",")
 
 ##### K-Nearest Neighbors #####
 
@@ -209,3 +211,6 @@ knn_preds <- predict(final_wf, new_data=amazon_test,type="prob") %>%
   rename(Action=.pred_1)
 
 vroom_write(x=knn_preds, file="./amazon_KNN.csv", delim=",")
+
+
+
